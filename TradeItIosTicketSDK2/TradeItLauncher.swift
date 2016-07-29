4609e9961c1f17189cf2f4b8b88e72b49543892a
Alex Kramer <alex@trade.it>
2016-07-29 18:32:47 -0400

    Adds TradeItLauncher and Welcome screen

---
+++ TradeItIosTicketSDK2/TradeItLauncher.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+@objc class TradeItLauncher: NSObject {
+    static func launchTradeItFromViewController(viewController: UIViewController) {
+        let storyboard = UIStoryboard(name: "TradeIt", bundle: NSBundle(identifier: "TradeIt.TradeItIosTicketSDK2") )
+        let navigationViewController = storyboard.instantiateViewControllerWithIdentifier("TRADE_IT_NAV_VIEW")
+        viewController.presentViewController(navigationViewController, animated: true, completion: nil)
+    }
+}

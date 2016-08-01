3a6d27b795e8c07d8b74200df6cb65f1d2e03de1
Alex Kramer <alex@trade.it>
2016-08-01 16:27:51 -0400

    Adds Quick and Nimble frameworks to project

--- TradeItIosTicketSDK2/TradeItLauncher.swift
+++ TradeItIosTicketSDK2/TradeItLauncher.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 @objc class TradeItLauncher: NSObject {
-    static func launchTradeItFromViewController(viewController: UIViewController) {
+    func launchTradeItFromViewController(viewController: UIViewController) {
         let storyboard = UIStoryboard(name: "TradeIt", bundle: NSBundle(identifier: "TradeIt.TradeItIosTicketSDK2") )
         let navigationViewController = storyboard.instantiateViewControllerWithIdentifier("TRADE_IT_NAV_VIEW")
         viewController.presentViewController(navigationViewController, animated: true, completion: nil)

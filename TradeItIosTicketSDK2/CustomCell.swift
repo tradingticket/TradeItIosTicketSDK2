e352474e88c8a3d27fb42093595fc58b06ccdef3
guillaumedebavelaere <guillaume@trade.it>
2016-08-12 18:46:08 -0400

    portfolio wipwipwipwip

---
+++ TradeItIosTicketSDK2/CustomCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    // MARK: Properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var value1Label: UILabel!
+    @IBOutlet weak var value2Label: UILabel!
+}
--- TradeItIosTicketSDK2/TradeItPortfolioViewController.swift
+++ TradeItIosTicketSDK2/TradeItPortfolioViewController.swift
@@ -1,12 +1,55 @@
 import UIKit
 
-class TradeItPortfolioViewController: UIViewController {
+class TradeItPortfolioViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     var accounts: [TradeItAccount] = []
+    
+    @IBOutlet weak var accountsTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("accounts available on portfolio screen: \(self.accounts)")
+        accountsTable.reloadData()
     }
+    
+   
+    
+    // MARK: UITableViewDelegate
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.accounts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "CUSTOM_CELL_ID"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as! CustomCell
+        
+        cell.nameLabel.text = getAccountName(self.accounts[indexPath.row], broker: "Dummy") //TODO get broker name
+        cell.value1Label.text = "" //TODO total value
+        cell.value2Label.text = "" //TODO buyingpower
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerCellIdentifier = "CUSTOM_HEADER_ID"
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(headerCellIdentifier)
+        return cell!
+    }
+    
+    
+    // MARK: private
+    private func getAccountName(account: TradeItAccount , broker: String) -> String {
+        let accountNumber = account.accountNumber
+        let startIndex = accountNumber.endIndex.advancedBy(-4)
+        return "\(broker) *\(String(accountNumber.characters.suffixFrom(startIndex)))"
+    }
+    
     
 }

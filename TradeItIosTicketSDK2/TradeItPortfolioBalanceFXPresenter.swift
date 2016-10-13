bee364f76374fda4060b8e311d0b0657f1ebeb97
Alex Kramer <alex@trade.it>
2016-10-13 11:44:53 -0400

    Fixes percent return calculation in PortfolioBalanceFXPresenter

--- TradeItIosTicketSDK2/TradeItPortfolioBalanceFXPresenter.swift
+++ TradeItIosTicketSDK2/TradeItPortfolioBalanceFXPresenter.swift
@@ -11,53 +11,59 @@ class TradeItPortfolioBalanceFXPresenter: TradeItPortfolioBalancePresenter  {
 
     func getFormattedTotalValue() -> String {
         guard let totalValueUSD = self.fxBalance?.totalValueUSD
-            else {return TradeItPresenter.MISSING_DATA_PLACEHOLDER}
+            else { return TradeItPresenter.MISSING_DATA_PLACEHOLDER }
 
         return formatCurrency(totalValueUSD)
     }
 
     func getUnrealizedProfitAndLoss() -> String {
         guard let unrealizedProfitAndLossBaseCurrency = self.fxBalance?.unrealizedProfitAndLossBaseCurrency
-            else {return TradeItPresenter.MISSING_DATA_PLACEHOLDER}
+            else { return TradeItPresenter.MISSING_DATA_PLACEHOLDER }
 
         return formatCurrency(unrealizedProfitAndLossBaseCurrency)
     }
     
     func getRealizedProfitAndLoss() -> String {
         guard let realizedProfitAndLossBaseCurrency = self.fxBalance?.realizedProfitAndLossBaseCurrency
-            else {return TradeItPresenter.MISSING_DATA_PLACEHOLDER}
+            else { return TradeItPresenter.MISSING_DATA_PLACEHOLDER }
 
         return formatCurrency(realizedProfitAndLossBaseCurrency)
     }
 
     func getMarginBalance() -> String {
         guard let marginBalanceBaseCurrency = self.fxBalance?.marginBalanceBaseCurrency
-            else {return TradeItPresenter.MISSING_DATA_PLACEHOLDER}
+            else { return TradeItPresenter.MISSING_DATA_PLACEHOLDER }
 
         return formatCurrency(marginBalanceBaseCurrency)
     }
 
-    func getFormattedTotalValueWithPercentage() -> String{
+    func getFormattedTotalValueWithPercentage() -> String {
         guard let totalValueBaseCurrency = fxBalance?.totalValueBaseCurrency
-            else { return TradeItPresenter.MISSING_DATA_PLACEHOLDER}
+            else { return TradeItPresenter.MISSING_DATA_PLACEHOLDER }
 
         var formattedTotalValue = NumberFormatter.formatCurrency(totalValueBaseCurrency)
 
-        let totalReturn = self.fxBalance?.unrealizedProfitAndLossBaseCurrency?.floatValue
-
-        if (totalReturn != nil && totalReturn != 0) {
-            let totalPercentReturn = totalReturn! / (totalValueBaseCurrency.floatValue - abs(totalReturn!))
-            formattedTotalValue += " (" + NumberFormatter.formatPercentage(totalPercentReturn) + ")"
-        }
+        formattedTotalValue += getFormattedTotalPercentage()
 
         return formattedTotalValue
     }
 
     func getFormattedBuyingPower() -> String {
         guard let buyingPowerBaseCurrency = self.fxBalance?.buyingPowerBaseCurrency
-            else { return TradeItPresenter.MISSING_DATA_PLACEHOLDER}
+            else { return TradeItPresenter.MISSING_DATA_PLACEHOLDER }
 
         return formatCurrency(buyingPowerBaseCurrency)
+    }
+
+    // MARK: Private
+
+    private func getFormattedTotalPercentage() -> String {
+        guard let totalValueBaseCurrency = fxBalance?.totalValueBaseCurrency?.floatValue
+            , let totalReturn = fxBalance?.unrealizedProfitAndLossBaseCurrency?.floatValue where totalReturn != 0
+            else { return "" }
+
+        let totalPercentReturn = totalReturn / (totalValueBaseCurrency - totalReturn)
+        return " (" + NumberFormatter.formatPercentage(totalPercentReturn) + ")"
     }
 
     private func formatCurrency(currency: NSNumber) -> String {

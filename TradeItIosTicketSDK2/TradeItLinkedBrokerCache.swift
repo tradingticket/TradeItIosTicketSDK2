f096bf690030c0374b5ef32d0762a7d5596320ad
Alex Kramer <alex@trade.it>
2016-10-25 00:24:12 -0400

    Implements TradeItLinkedBrokerCache

--- TradeItIosTicketSDK2/TradeItLinkedBrokerCache.swift
+++ TradeItIosTicketSDK2/TradeItLinkedBrokerCache.swift
@@ -1,3 +1,98 @@
-class TradeItLinkedBrokerCache {
+import SwiftyUserDefaults
 
+// https://github.com/radex/SwiftyUserDefaults
+
+extension DefaultsKeys {
+    static let linkedBrokerCache = DefaultsKey<NSDictionary?>("linkedBrokerCache")
+}
+
+class TradeItLinkedBrokerCache {
+    typealias UserId = String
+    typealias AccountNumber = String
+    typealias SerializedLinkedBroker = [String: AnyObject]
+    typealias SerializedLinkedBrokers = [UserId: SerializedLinkedBroker]
+    typealias SerializedLinkedBrokerAccount = [String: String]
+    typealias SerializedLinkedBrokerAccounts = [AccountNumber: SerializedLinkedBrokerAccount]
+
+    private let ACCOUNTS_KEY = "ACCOUNTS"
+    private let ACCOUNTS_LAST_UPDATED_KEY = "ACCOUNTS_LAST_UPDATED"
+    private let ACCOUNT_NAME_KEY = "ACCOUNT_NAME"
+
+    var defaults = NSUserDefaults(suiteName: "it.trade")!
+
+    func cache(linkedBroker linkedBroker: TradeItLinkedBroker) {
+        guard let userId = linkedBroker.linkedLogin.userId else { return }
+
+        var linkedBrokerCache = defaults[.linkedBrokerCache] as? SerializedLinkedBrokers ?? SerializedLinkedBrokers()
+
+        let serializedLinkedBroker = serialize(linkedBroker: linkedBroker)
+
+        linkedBrokerCache[userId] = serializedLinkedBroker
+
+        defaults[.linkedBrokerCache] = linkedBrokerCache
+    }
+
+    func syncFromCache(linkedBroker linkedBroker: TradeItLinkedBroker) {
+        guard let userId = linkedBroker.linkedLogin.userId
+            , let linkedBrokerCache = defaults[.linkedBrokerCache] as? SerializedLinkedBrokers
+            , let serializedLinkedBroker = linkedBrokerCache[userId] as SerializedLinkedBroker?
+        else { return }
+
+        if let serializedAccounts = serializedLinkedBroker[ACCOUNTS_KEY] as? SerializedLinkedBrokerAccounts {
+            let accounts = deserialize(serializedAccounts: serializedAccounts,
+                                       forLinkedBroker: linkedBroker)
+
+            linkedBroker.accounts = accounts
+        }
+
+//        linkedBroker.accountsLastUpdated = serializedLinkedBroker[ACCOUNTS_LAST_UPDATED_KEY] ?? 0
+    }
+
+    func remove(linkedBroker linkedBroker: TradeItLinkedBroker) {
+        guard let userId = linkedBroker.linkedLogin.userId
+            , var linkedBrokerCache = defaults[.linkedBrokerCache] as? SerializedLinkedBrokers
+            else { return }
+
+        linkedBrokerCache[userId] = nil
+    }
+
+    // MARK: Private
+
+    private func serialize(linkedBroker linkedBroker: TradeItLinkedBroker) -> SerializedLinkedBroker {
+        return [
+            ACCOUNTS_LAST_UPDATED_KEY: 0, // linkedBroker.accountsLastUpdated // NSDate().timeIntervalSince1970
+            ACCOUNTS_KEY: serialize(accounts: linkedBroker.accounts)
+        ]
+    }
+
+    private func deserialize(serializedAccounts serializedAccounts: SerializedLinkedBrokerAccounts,
+                             forLinkedBroker linkedBroker: TradeItLinkedBroker) -> [TradeItLinkedBrokerAccount] {
+        var accounts = [TradeItLinkedBrokerAccount]()
+
+        for (accountNumber, serializedAccount) in serializedAccounts {
+            let account = TradeItLinkedBrokerAccount(linkedBroker: linkedBroker,
+                                                     accountName: serializedAccount[ACCOUNT_NAME_KEY] ?? "",
+                                                     accountNumber: accountNumber,
+                                                     balance: nil,
+                                                     fxBalance: nil,
+                                                     positions: [])
+            accounts.append(account)
+        }
+
+        return accounts
+    }
+
+    private func serialize(accounts accounts: [TradeItLinkedBrokerAccount]) -> SerializedLinkedBrokerAccounts {
+        var serializedAccounts = SerializedLinkedBrokerAccounts()
+
+        for account in accounts {
+            var serializedAccount = SerializedLinkedBrokerAccount()
+
+            serializedAccount[ACCOUNT_NAME_KEY] = account.accountName
+
+            serializedAccounts[account.accountNumber] = serializedAccount
+        }
+
+        return serializedAccounts
+    }
 }
--- TradeItIosTicketSDK2Tests/TradeItLinkedBrokerCacheSpec.swift
+++ TradeItIosTicketSDK2Tests/TradeItLinkedBrokerCacheSpec.swift
@@ -1,39 +1,163 @@
 import Quick
 import Nimble
+import SwiftyUserDefaults
+@testable import TradeItIosTicketSDK2
 
 class TradeItLinkedBrokerCacheSpec: QuickSpec {
+    let defaults = NSUserDefaults(suiteName: "TEST")!
+
     override func spec() {
-        var cache: TradeItLinkedBrokerCache
+        var cache: TradeItLinkedBrokerCache!
+        var linkedLogin1: TradeItLinkedLogin!
+        var linkedLogin2: TradeItLinkedLogin!
+        var linkedBroker1: TradeItLinkedBroker!
+        var linkedBroker2: TradeItLinkedBroker!
 
         beforeEach {
-            // TODO: CLEAR NSUSERDEFAULTS
+            self.defaults.removeAll()
+            expect(Defaults[.linkedBrokerCache]).to(beNil())
+
             cache = TradeItLinkedBrokerCache()
+            cache.defaults = self.defaults
+
+            linkedLogin1 = TradeItLinkedLogin(
+                label: "My Special Label 1",
+                broker: "My Special Broker 1",
+                userId: "My Special User ID 1",
+                andKeyChainId: "My Special Keychain ID 1")
+
+            linkedLogin2 = TradeItLinkedLogin(
+                label: "My Special Label 2",
+                broker: "My Special Broker 2",
+                userId: "My Special User ID 2",
+                andKeyChainId: "My Special Keychain ID 2")
+
+            linkedBroker1 = TradeItLinkedBroker(session: FakeTradeItSession(), linkedLogin: linkedLogin1)
+            linkedBroker2 = TradeItLinkedBroker(session: FakeTradeItSession(), linkedLogin: linkedLogin2)
+
+            let account1 = TradeItLinkedBrokerAccount(
+                linkedBroker: linkedBroker1,
+                accountName: "My Special Account 1",
+                accountNumber: "My Special Account Number 1",
+                balance: nil,
+                fxBalance: nil,
+                positions: [])
+
+            linkedBroker1.accounts.append(account1)
+
+            let account2 = TradeItLinkedBrokerAccount(
+                linkedBroker: linkedBroker1,
+                accountName: "My Special Account 2",
+                accountNumber: "My Special Account Number 2",
+                balance: nil,
+                fxBalance: nil,
+                positions: [])
+
+            linkedBroker1.accounts.append(account2)
+
+            let account3 = TradeItLinkedBrokerAccount(
+                linkedBroker: linkedBroker2,
+                accountName: "My Special Account 3",
+                accountNumber: "My Special Account Number 3",
+                balance: nil,
+                fxBalance: nil,
+                positions: [])
+
+            linkedBroker2.accounts.append(account3)
         }
 
-        describe("cacheLinkedBroker") {
-            context("") {
-                it("") {
-                    expect(linkedBrokerManager.linkedBrokers).to(beEmpty())
+        describe("cache(linkedBroker:)") {
+            context("when the broker doesn't already exist in the cache") {
+                beforeEach {
+                    //////
+                    let serializedBrokers = self.defaults[.linkedBrokerCache] as? TradeItLinkedBrokerCache.SerializedLinkedBrokerCache
+                    print("=====> serializedBrokers BEFORE FIRST ADD: \(serializedBrokers)")
+                    /////
+                    cache.cache(linkedBroker: linkedBroker1)
+                }
+
+                it("adds the broker to the cache") {
+                    let serializedBrokers = self.defaults[.linkedBrokerCache] as! TradeItLinkedBrokerCache.SerializedLinkedBrokerCache
+
+                    print("=====> serializedBrokers AFTER FIRST ADD: \(serializedBrokers)")
+                    expect(serializedBrokers.keys.count).to(equal(1))
+
+                    let accounts = serializedBrokers["My Special User ID 1"] as TradeItLinkedBrokerCache.SerializedLinkedBrokerAccounts!
+
+                    expect(accounts.count).to(equal(2))
+
+                    let serializedAccount1 = accounts["My Special Account Number 1"]!
+
+                    expect(serializedAccount1["accountName"]!).to(equal("My Special Account 1"))
+
+                    let serializedAccount2 = accounts["My Special Account Number 2"]!
+
+                    expect(serializedAccount2["accountName"]!).to(equal("My Special Account 2"))
+                }
+
+                describe ("adding a subsequent linked broker") {
+                    beforeEach {
+                        cache.cache(linkedBroker: linkedBroker2)
+                    }
+
+                    it("adds it to the cache") {
+                        let serializedBrokers = self.defaults[.linkedBrokerCache] as! TradeItLinkedBrokerCache.SerializedLinkedBrokerCache
+
+                        print("=====> serializedBrokers AFTER SECOND ADD: \(serializedBrokers)")
+                        expect(serializedBrokers.keys.count).to(equal(2))
+
+                        let accounts1 = serializedBrokers["My Special User ID 1"] as TradeItLinkedBrokerCache.SerializedLinkedBrokerAccounts!
+
+                        expect(accounts1.count).to(equal(2))
+
+                        let accounts2 = serializedBrokers["My Special User ID 2"] as TradeItLinkedBrokerCache.SerializedLinkedBrokerAccounts!
+
+                        expect(accounts2.count).to(equal(1))
+
+                        let serializedAccount3 = accounts2["My Special Account Number 3"]!
+
+                        expect(serializedAccount3["accountName"]!).to(equal("My Special Account 3"))
+                    }
                 }
             }
 
-            context("") {
-                it("") {
+            context("when the broker already exists in the cache") {
+                beforeEach {
+                    cache.cache(linkedBroker: linkedBroker1)
+                    cache.cache(linkedBroker: linkedBroker2)
 
+                    let newAccount = TradeItLinkedBrokerAccount(
+                        linkedBroker: linkedBroker1,
+                        accountName: "My New Account",
+                        accountNumber: "My New Account Number",
+                        balance: nil,
+                        fxBalance: nil,
+                        positions: [])
+
+                    linkedBroker1.accounts = [newAccount]
+
+                    cache.cache(linkedBroker: linkedBroker1)
+                }
+
+                it("overwrites all of the broker's cached data") {
+                    let serializedBrokers = self.defaults[.linkedBrokerCache] as! TradeItLinkedBrokerCache.SerializedLinkedBrokerCache
+
+                    print("=====> serializedBrokers AFTER REWRITE: \(serializedBrokers)")
+                    expect(serializedBrokers.keys.count).to(equal(2))
+
+                    let accounts = serializedBrokers["My Special User ID 1"] as TradeItLinkedBrokerCache.SerializedLinkedBrokerAccounts!
+
+                    expect(accounts.count).to(equal(1))
+
+                    let serializedAccount = accounts["My New Account Number"]!
+
+                    expect(serializedAccount["accountName"]!).to(equal("My New Account"))
                 }
             }
         }
 
-        describe("loadLinkedBrokerFromCache") {
+        describe("syncFromCache(linkedBroker:)") {}
 
-        }
-
-        describe("clearLinkedBrokerFromCache") {
-
-        }
-
-        describe("clearCache") {
-
-        }
+        describe("remove(linkedBroker:)") {}
     }
 }

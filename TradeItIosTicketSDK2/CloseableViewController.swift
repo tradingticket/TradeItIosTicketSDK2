7e055bffbcdabd0def1d5538b5b2e5effd868d4f
Alex Kramer <alex@trade.it>
2017-01-23 20:27:44 -0500

    Extracts CloseableViewController from TradeItViewController

---
+++ TradeItIosTicketSDK2/CloseableViewController.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+class CloseableViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureNavigationItem()
+    }
+
+    func configureNavigationItem() {
+        guard let viewControllers = self.navigationController?.viewControllers else {
+            self.createCloseButton()
+            return
+        }
+
+        if viewControllers.count == 1 {
+            self.createCloseButton()
+        }
+    }
+
+    func createCloseButton() {
+        let closeButtonItem = UIBarButtonItem(title: "Close", style: UIBarButtonItemStyle.plain, target: self, action: #selector(closeButtonWasTapped(_:)))
+
+        self.navigationItem.rightBarButtonItem = closeButtonItem
+    }
+
+    func closeButtonWasTapped(_ sender: UIBarButtonItem) {
+        if let viewControllers = self.navigationController?.viewControllers , viewControllers.count > 1 {
+            _ = self.navigationController?.popViewController(animated: true)
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+}

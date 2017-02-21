96467fb62d6b9eb7ccfc555eb4e2d9bab50c3dd3
guillaumedebavelaere <guillaume@trade.it>
2017-02-21 11:32:30 -0500

    [#139297907] set dummy string as default token

--- TradeItIosTicketSDK2/TradeItLinkedBrokerManager.swift
+++ TradeItIosTicketSDK2/TradeItLinkedBrokerManager.swift
@@ -326,7 +326,7 @@ import PromiseKit
     private func loadLinkedBrokerFromLinkedLogin(_ linkedLogin: TradeItLinkedLogin) -> TradeItLinkedBroker {
         let tradeItSession = sessionProvider.provide(connector: self.connector)
         //provides a default token, so if the user doesn't authenticate before an other call, it will pass an expired token in order to get the session expired error
-        tradeItSession?.token = "dd61aa94fa094e6ab54fa4b31853bbd4"
+        tradeItSession?.token = "trade-it-fetch-fresh-token"
         return TradeItLinkedBroker(session: tradeItSession!, linkedLogin: linkedLogin)
     }
 

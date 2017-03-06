761997b14afbd62e73f1c25e2034f52fd6793d83
Alex Kramer <alex@trade.it>
2017-03-06 17:24:43 -0500

    Yahoo: Implements submitting order on trade preview screen

--- TradeItIosTicketSDK2/TradeItYahooTradePreviewViewController.swift
+++ TradeItIosTicketSDK2/TradeItYahooTradePreviewViewController.swift
@@ -1,29 +1,125 @@
 import UIKit
+import MBProgressHUD
 
 class TradeItYahooTradePreviewViewController: CloseableViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var brokerLabel: UILabel!
     @IBOutlet weak var orderDetailsTable: UITableView!
-    @IBOutlet weak var submitOrderButton: UIButton!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var statusLabel: UILabel!
 
     var linkedBrokerAccount: TradeItLinkedBrokerAccount!
     var previewOrderResult: TradeItPreviewOrderResult?
+    var placeOrderResult: TradeItPlaceOrderResult?
     var placeOrderCallback: TradeItPlaceOrderHandlers?
     var previewCellData = [PreviewCellData]()
-//    var acknowledgementCellData: [AcknowledgementCellData] = []
+    //    var acknowledgementCellData: [AcknowledgementCellData] = []
     var alertManager = TradeItAlertManager()
+    weak var delegate: TradeItYahooTradePreviewViewControllerDelegate?
+
+    private let actionButtonTitleTextSubmitOrder = "Submit order"
+    private let actionButtonTitleTextGoToPortolio = "Go to portfolio"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        precondition(self.linkedBrokerAccount != nil, "ERROR: TradeItYahooTradingPreviewViewController loaded without setting linkedBrokerAccount.")
+        precondition(self.linkedBrokerAccount != nil, "TradeItSDK ERROR: TradeItYahooTradingPreviewViewController loaded without setting linkedBrokerAccount.")
 
         self.title = "Preview order"
         self.brokerLabel.text = self.linkedBrokerAccount.brokerName
+        self.statusLabel.text = "Preview order"
+        self.statusLabel.textColor = UIColor.yahooTextColor
+        self.actionButton.setTitle(self.actionButtonTitleTextSubmitOrder, for: .normal)
 
         self.previewCellData = self.generatePreviewCellData()
 
         orderDetailsTable.dataSource = self
         orderDetailsTable.delegate = self
+    }
+
+    private func updateOrderDetailsTable() {
+        self.previewCellData = self.generatePreviewCellData()
+        self.orderDetailsTable.reloadData()
+    }
+
+    // MARK: IBActions
+
+    private func submitOrder() {
+        guard let placeOrderCallback = self.placeOrderCallback else {
+            print("TradeItSDK ERROR: placeOrderCallback not set on TradeItYahooTradePreviewViewController")
+            return
+        }
+
+        self.actionButton.disable()
+
+        let activityView = MBProgressHUD.showAdded(to: self.view, animated: true)
+        activityView.label.text = "Authenticating"
+
+        self.linkedBrokerAccount?.linkedBroker?.authenticateIfNeeded(
+            onSuccess: {
+                activityView.label.text = "Placing Order"
+
+                placeOrderCallback(
+                    { placeOrderResult in
+
+                        self.placeOrderResult = placeOrderResult
+
+                        self.title = "Order Confirmation"
+
+                        self.statusLabel.text = "✓ Order Submitted"
+                        self.statusLabel.textColor = UIColor.yahooGreenSuccessColor
+
+                        self.actionButton.enable()
+                        self.actionButton.setTitle(self.actionButtonTitleTextGoToPortolio, for: .normal)
+
+                        self.updateOrderDetailsTable()
+
+                        activityView.hide(animated: true)
+                    },
+                    { error in
+                        activityView.hide(animated: true)
+
+                        self.actionButton.enable()
+
+                        guard let linkedBroker = self.linkedBrokerAccount.linkedBroker else {
+                            return self.alertManager.showError(
+                                error,
+                                onViewController: self
+                            )
+                        }
+
+                        self.alertManager.showRelinkError(
+                            error,
+                            withLinkedBroker: linkedBroker,
+                            onViewController: self,
+                            onFinished: {}
+                        )
+                    }
+                )
+            },
+            onSecurityQuestion: { securityQuestion, answerSecurityQuestion, cancelSecurityQuestion in
+                activityView.hide(animated: true)
+                self.alertManager.promptUserToAnswerSecurityQuestion(
+                    securityQuestion,
+                    onViewController: self,
+                    onAnswerSecurityQuestion: answerSecurityQuestion,
+                    onCancelSecurityQuestion: cancelSecurityQuestion
+                )
+            },
+            onFailure: { errorResult in
+                activityView.hide(animated: true)
+                self.actionButton.enable()
+                // TODO: use self.alertManager.showRelinkError
+                self.alertManager.showError(errorResult, onViewController: self)
+            }
+        )
+    }
+
+    @IBAction func actionButtonTapped(_ sender: UIButton) {
+        if let placeOrderResult = self.placeOrderResult {
+            self.delegate?.viewPortfolioTapped(onTradePreviewViewController: self, linkedBrokerAccount: self.linkedBrokerAccount)
+        } else {
+            self.submitOrder()
+        }
     }
 
     // MARK: UITableViewDelegate
@@ -41,14 +137,14 @@ class TradeItYahooTradePreviewViewController: CloseableViewController, UITableVi
         let cellData = previewCellData[indexPath.row]
 
         switch cellData {
-//        case let warningCellData as WarningCellData:
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "PREVIEW_ORDER_WARNING_CELL_ID") as! TradeItPreviewOrderWarningTableViewCell
-//            cell.populate(withWarning: warningCellData.warning)
-//            return cell
-//        case let acknowledgementCellData as AcknowledgementCellData:
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "PREVIEW_ORDER_ACKNOWLEDGEMENT_CELL_ID") as! TradeItPreviewOrderAcknowledgementTableViewCell
-//            cell.populate(withCellData: acknowledgementCellData, andDelegate: self)
-//            return cell
+            //        case let warningCellData as WarningCellData:
+            //            let cell = tableView.dequeueReusableCell(withIdentifier: "PREVIEW_ORDER_WARNING_CELL_ID") as! TradeItPreviewOrderWarningTableViewCell
+            //            cell.populate(withWarning: warningCellData.warning)
+            //            return cell
+            //        case let acknowledgementCellData as AcknowledgementCellData:
+            //            let cell = tableView.dequeueReusableCell(withIdentifier: "PREVIEW_ORDER_ACKNOWLEDGEMENT_CELL_ID") as! TradeItPreviewOrderAcknowledgementTableViewCell
+            //            cell.populate(withCellData: acknowledgementCellData, andDelegate: self)
+        //            return cell
         case let valueCellData as ValueCellData:
             let cell = tableView.dequeueReusableCell(withIdentifier: "TRADE_IT_YAHOO_PREVIEW_CELL_ID") ?? UITableViewCell()
             cell.textLabel?.text = valueCellData.label
@@ -60,15 +156,6 @@ class TradeItYahooTradePreviewViewController: CloseableViewController, UITableVi
         }
     }
 
-//    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return UITableViewAutomaticDimension
-//    }
-//
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return UITableViewAutomaticDimension
-//    }
-
-
     // MARK: Private
 
     private func generatePreviewCellData() -> [PreviewCellData] {
@@ -76,14 +163,25 @@ class TradeItYahooTradePreviewViewController: CloseableViewController, UITableVi
             let orderDetails = previewOrderResult?.orderDetails
             else { return [] }
 
-        var cells: [PreviewCellData] = []
+        var cells = [PreviewCellData]()
 
-//        cells += generateWarningCellData()
+        //        cells += generateWarningCellData()
 
-//        acknowledgementCellData = generateAcknowledgementCellData()
-//        cells += acknowledgementCellData as [PreviewCellData]
+        //        acknowledgementCellData = generateAcknowledgementCellData()
+        //        cells += acknowledgementCellData as [PreviewCellData]
+
+        cells += [
+            ValueCellData(label: "Account", value: linkedBrokerAccount.getFormattedAccountName())
+        ] as [PreviewCellData]
 
         let orderDetailsPresenter = TradeItOrderDetailsPresenter(orderDetails: orderDetails)
+
+        if let orderNumber = self.placeOrderResult?.orderNumber {
+            cells += [
+                ValueCellData(label: "Order #", value: orderNumber)
+            ] as [PreviewCellData]
+        }
+
         cells += [
             ValueCellData(label: "Account", value: linkedBrokerAccount.getFormattedAccountName()),
             ValueCellData(label: "Symbol", value: orderDetails.orderSymbol),
@@ -119,4 +217,11 @@ class TradeItYahooTradePreviewViewController: CloseableViewController, UITableVi
     private func formatCurrency(_ value: NSNumber) -> String {
         return NumberFormatter.formatCurrency(value, currencyCode: TradeItPresenter.DEFAULT_CURRENCY_CODE)
     }
+}
+
+protocol TradeItYahooTradePreviewViewControllerDelegate: class {
+    func viewPortfolioTapped(
+        onTradePreviewViewController tradePreviewViewController: TradeItYahooTradePreviewViewController,
+        linkedBrokerAccount: TradeItLinkedBrokerAccount
+    )
 }

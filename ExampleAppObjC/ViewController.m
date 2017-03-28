de1e5765b496132f359ea81c6dd203d4f793817d
James Robert Somers <james@trade.it>
2017-03-28 15:39:53 -0400

    Add ExampleAppObjC target and link to SDK

---
+++ ExampleAppObjC/ViewController.m
@@ -0,0 +1,22 @@
+#import "ViewController.h"
+#import <TradeItIosTicketSDK2/TradeItIosTicketSDK2-Swift.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ExampleAppObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ExampleAppObjC
+//
+//  Created by James Robert Somers on 28/3/17.
+//  Copyright © 2017 TradeIt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

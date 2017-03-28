fa7483f91c18fe1d6748feddd767ce806fdfcfc3
James Robert Somers <james@trade.it>
2017-03-28 18:04:57 -0400

    Remove cruft

--- ExampleAppObjC/AppDelegate.h
+++ ExampleAppObjC/AppDelegate.h
@@ -1,17 +1,7 @@
-//
-//  AppDelegate.h
-//  ExampleAppObjC
-//
-//  Created by James Robert Somers on 28/3/17.
-//  Copyright © 2017 TradeIt. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
-
 @end
-
--- ExampleAppObjC/ViewController.h
+++ ExampleAppObjC/ViewController.h
@@ -1,15 +1,5 @@
-//
-//  ViewController.h
-//  ExampleAppObjC
-//
-//  Created by James Robert Somers on 28/3/17.
-//  Copyright © 2017 TradeIt. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
 
-
 @end
-

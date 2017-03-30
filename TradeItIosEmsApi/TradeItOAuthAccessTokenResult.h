d68b7c95709f3b84502b479e3bd9e502baedd3e5
guillaumedebavelaere <guillaume@trade.it>
2017-03-30 17:39:25 -0400

    adds activationTime to TradeItOAuthAccessTokenResult

--- TradeItIosEmsApi/TradeItOAuthAccessTokenResult.h
+++ TradeItIosEmsApi/TradeItOAuthAccessTokenResult.h
@@ -3,5 +3,6 @@
 @interface TradeItOAuthAccessTokenResult: TradeItAuthLinkResult
 
 @property (nullable) NSString *broker;
+@property (nullable) NSString *activationTime; // IMMEDIATE if there is no delay using the linked account, otherwise: ONE_OR_TWO_BUSINESS_DAY
 
 @end

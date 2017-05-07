db9d58059a108adb9ecc146ccd7bb26fa33cc925
Alex Kramer <alex@trade.it>
2017-05-07 18:36:24 -0400

    Renames TradeItQuotePresenter methods get*Label -> get*LabelText

--- TradeItIosTicketSDK2/TradeItSubtitleWithDetailsCellTableViewCell.swift
+++ TradeItIosTicketSDK2/TradeItSubtitleWithDetailsCellTableViewCell.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+// TODO: If this is specific to displaying only quotes, it should have a less generic name
+//       OR it should be subclassed to a more specific quote use case.
 class TradeItSubtitleWithDetailsCellTableViewCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var subtitleLabel: UILabel!
@@ -14,9 +16,9 @@ class TradeItSubtitleWithDetailsCellTableViewCell: UITableViewCell {
         self.titleLabel?.text = self.textLabel?.text
         self.textLabel?.text = nil
         if let quotePresenter = quotePresenter {
-            self.subtitleLabel?.text = quotePresenter.getTimestampLabel()
-            self.detailsLabel?.text = quotePresenter.getLastPriceLabel()
-            self.subtitleDetailsLabel?.text = quotePresenter.getChangeLabel()
+            self.subtitleLabel?.text = quotePresenter.getTimestampLabelText()
+            self.detailsLabel?.text = quotePresenter.getLastPriceLabelText()
+            self.subtitleDetailsLabel?.text = quotePresenter.getChangeLabelText()
             self.subtitleDetailsLabel.textColor = quotePresenter.getChangeLabelColor()
         } else {
             self.subtitleLabel?.text = TradeItPresenter.MISSING_DATA_PLACEHOLDER

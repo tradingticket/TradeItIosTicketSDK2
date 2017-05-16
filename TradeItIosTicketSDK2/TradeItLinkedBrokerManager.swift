ae5838db688bf185cf50c58a9833240b0f639d89
Alex Kramer <alex@trade.it>
2017-05-15 22:03:01 -0400

    Adds logging of linkedBroker.error to LinkedBrokerManager.printLinkedBrokers

--- TradeItIosTicketSDK2/TradeItLinkedBrokerManager.swift
+++ TradeItIosTicketSDK2/TradeItLinkedBrokerManager.swift
@@ -414,7 +414,7 @@ import PromiseKit
             let linkedLogin = linkedBroker.linkedLogin
             let userToken = TradeItSDK.linkedBrokerManager.connector.userToken(fromKeychainId: linkedLogin.keychainId)
 
-            print("=====> \(linkedBroker.brokerName)(\(linkedBroker.accounts.count) accounts)\n    accountsUpdated: \(String(describing: linkedBroker.accountsLastUpdated))\n    userId: \(linkedLogin.userId ?? "MISSING USER ID")\n    keychainId: \(linkedLogin.keychainId ?? "MISSING KEYCHAIN ID")\n    userToken: \(userToken ?? "MISSING USER TOKEN")")
+            print("=====> \(linkedBroker.brokerName)(\(linkedBroker.accounts.count) accounts)\n    accountsUpdated: \(String(describing: linkedBroker.accountsLastUpdated))\n    userId: \(linkedLogin.userId ?? "MISSING USER ID")\n    keychainId: \(linkedLogin.keychainId ?? "MISSING KEYCHAIN ID")\n    userToken: \(userToken ?? "MISSING USER TOKEN")\n    error: \(String(describing: linkedBroker.error?.errorCode)) - \(String(describing: linkedBroker.error?.shortMessage)) - \(String(describing: linkedBroker.error?.longMessages?.first))")
 
             print("        === ACCOUNTS ===")
 

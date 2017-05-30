c0eaed2183665a9042fa922c12b2f9af56cb8df9
James Robert Somers <james@trade.it>
2017-05-30 15:58:59 -0400

    Remove duplicated themes in menus

--- ExampleApp/ExampleViewController.swift
+++ ExampleApp/ExampleViewController.swift
@@ -64,35 +64,6 @@ class ExampleViewController: UIViewController, UITableViewDataSource, UITableVie
                 ]
             ),
             Section(
-                label: "THEMES",
-                actions: [
-                    Action(
-                        label: "setLightTheme",
-                        action: {
-                            TradeItSDK.theme = TradeItTheme.light()
-                            self.handleThemeChange()
-                        }
-                    ),
-                    Action(
-                        label: "setDarkTheme",
-                        action: {
-                            TradeItSDK.theme = TradeItTheme.dark()
-                            self.handleThemeChange()
-                        }
-                    ),
-                    Action(
-                        label: "setCustomTheme",
-                        action: {
-                            let customTheme = TradeItTheme()
-                            customTheme.backgroundColor = UIColor(red: 0.8275, green: 0.9176, blue: 1, alpha: 1.0)
-                            customTheme.tableHeaderBackgroundColor = UIColor(red: 0.4784, green: 0.7451, blue: 1, alpha: 1.0)
-                            TradeItSDK.theme = customTheme
-                            self.handleThemeChange()
-                        }
-                    )
-                ]
-            ),
-            Section(
                 label: "SETTINGS",
                 actions: [
                     Action(

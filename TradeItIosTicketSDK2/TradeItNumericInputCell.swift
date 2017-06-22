963812496f973a0bb23e289e565ae2d6fe367533
Alex Kramer <alex@trade.it>
2017-06-21 20:43:37 -0400

    Fixes indentation and naming

--- TradeItIosTicketSDK2/TradeItNumericInputCell.swift
+++ TradeItIosTicketSDK2/TradeItNumericInputCell.swift
@@ -2,56 +2,67 @@ import UIKit
 
 class TradeItNumericInputCell: UITableViewCell {
     @IBOutlet weak var textField: TradeItNumberField!
-    
+
     var onValueUpdated: ((_ newValue: NSDecimalNumber?) -> Void)?
-    
+
     override func awakeFromNib() {
         self.textField.padding = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
-        self.addDoneButtonOnKeyboard()
+        self.addDoneButtonToKeyboard()
     }
-    
-    
-    
-    func configure(initialValue: NSDecimalNumber?,
-                   placeholderText: String,
-                   onValueUpdated: @escaping (_ newValue: NSDecimalNumber?) -> Void) {
+
+    func configure(
+        initialValue: NSDecimalNumber?,
+        placeholderText: String,
+        onValueUpdated: @escaping (_ newValue: NSDecimalNumber?) -> Void
+    ) {
         self.onValueUpdated = onValueUpdated
         self.textField.placeholder = placeholderText
-        
+
         if let initialValue = initialValue {
             self.textField.text = "\(initialValue)"
         } else {
             self.textField.text = nil
         }
     }
-    
-    func addDoneButtonOnKeyboard(){
+
+    func dismissKeyboard() {
+        self.textField.resignFirstResponder()
+    }
+
+    // MARK: Private
+
+    private func addDoneButtonToKeyboard() {
         let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
         doneToolbar.barStyle = UIBarStyle.default
-        let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
-        
-        let done: UIBarButtonItem  = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.done, target: self, action: #selector(self.doneButtonAction))
-        
-        var items = [UIBarButtonItem]()
-        items.append(flexSpace)
-        items.append(done)
-        
-        doneToolbar.items = items
+
+        let flexSpace = UIBarButtonItem(
+            barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace,
+            target: nil,
+            action: nil
+        )
+
+        let doneBarButtonItem: UIBarButtonItem  = UIBarButtonItem(
+            title: "Done",
+            style: UIBarButtonItemStyle.done,
+            target: self,
+            action: #selector(self.dismissKeyboard)
+        )
+
+        var barButtonItems = [UIBarButtonItem]()
+        barButtonItems.append(flexSpace)
+        barButtonItems.append(doneBarButtonItem)
+
+        doneToolbar.items = barButtonItems
         doneToolbar.sizeToFit()
-        
-        textField.inputAccessoryView = doneToolbar
+
+        self.textField.inputAccessoryView = doneToolbar
     }
-    
-    func doneButtonAction(){
-        textField.resignFirstResponder()
-    }
-    
-    
+
     // MARK: IBActions
-    
+
     @IBAction func textFieldDidChange(_ sender: TradeItNumberField) {
         let numericValue = NSDecimalNumber.init(string: sender.text)
-        
+
         if numericValue == NSDecimalNumber.notANumber {
             self.onValueUpdated?(nil)
         } else {

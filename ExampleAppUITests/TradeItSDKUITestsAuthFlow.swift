3ab06114680abbdc558dc5b63d4fbb84253f498a
guillaumedebavelaere <guillaume@trade.it>
2017-07-26 17:50:36 -0400

    UI Testss - fix auth flow

--- ExampleAppUITests/TradeItSDKUITestsAuthFlow.swift
+++ ExampleAppUITests/TradeItSDKUITestsAuthFlow.swift
@@ -18,7 +18,7 @@ class TradeItSDKUITestsAuthFlow: XCTestCase {
     
     func testSecurityQuestionFlow() {
         clearData(app)
-        handleWelcomeScreen(app, launchOption: "launchPortfolio")
+        handleWelcomeScreen(app, launchOption: "Portfolio")
         selectBrokerFromTheBrokerSelectionScreen(app, longBrokerName: "Dummy Broker")
         submitValidCredentialsOnTheLoginScreen(app, longBrokerName: "Dummy Broker", username: "dummySecurity")
         
@@ -28,24 +28,19 @@ class TradeItSDKUITestsAuthFlow: XCTestCase {
         securityQuestionAlert.textFields[""].typeText("123")
         securityQuestionAlert.buttons["Submit"].tap()
         waitForElementToAppear(securityQuestionAlert)
-        securityQuestionAlert.textFields[""].typeText("")
-        securityQuestionAlert.buttons["Submit"].tap()
-        waitForElementToAppear(app.alerts["Could Not Login"])
-        app.alerts["Could Not Login"].buttons["OK"].tap()
-        app.buttons["Link Broker"].tap()
-        waitForElementToAppear(securityQuestionAlert)
         securityQuestionAlert.textFields[""].typeText("tradingticket")
         securityQuestionAlert.buttons["Submit"].tap()
+        completeOauthScreen(app)
         waitForElementToAppear(app.navigationBars["Portfolio"])
     }
     
     func testDummyOptionFlow(){
         clearData(app)
-        handleWelcomeScreen(app, launchOption: "launchPortfolio")
+        handleWelcomeScreen(app, launchOption: "Portfolio")
         selectBrokerFromTheBrokerSelectionScreen(app, longBrokerName: "Dummy Broker")
         submitValidCredentialsOnTheLoginScreen(app, longBrokerName: "Dummy Broker", username: "dummyOption")
         
-        //Test for option pop-up, wrong answer first, then the correct one
+        //Test for option pop-up, wrong answer first, then cancel
         waitForElementToAppear(app.alerts["Security Question"])
         let alert = app.alerts["Security Question"]
         XCTAssert(alert.buttons["Cancel"].exists)
@@ -58,11 +53,7 @@ class TradeItSDKUITestsAuthFlow: XCTestCase {
         XCTAssert(app.alerts["Authentication failed"].staticTexts["The security question was canceled."].exists)
         app.alerts["Authentication failed"].buttons["OK"].tap()
         waitForElementToDisappear(app.alerts["Authentication failed"])
-        app.buttons["Link Broker"].tap()
-        waitForElementToAppear(alert)
-        alert.buttons["option 2"].tap()
-        waitForElementToAppear(alert)
-        alert.buttons["option 1"].tap()
+        completeOauthScreen(app)
         waitForElementToAppear(app.navigationBars["Portfolio"])
     }
 }

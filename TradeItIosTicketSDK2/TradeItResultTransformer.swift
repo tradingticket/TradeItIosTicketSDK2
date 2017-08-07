6d42f7e927a021b2a4c7d5d6a9055fabfb6f1893
James Robert Somers <james@trade.it>
2017-08-07 11:31:31 -0400

    Tidy up ResultTransformer

--- TradeItIosTicketSDK2/TradeItResultTransformer.swift
+++ TradeItIosTicketSDK2/TradeItResultTransformer.swift
@@ -1,22 +1,20 @@
 class TradeItResultTransformer {
-    static let JSON_ERROR = TradeItErrorResult.error(withSystemMessage: "Error parsing JSON response")
-
     static func transform<T: TradeItResult>(targetClassType: T.Type, json: String?) -> T? {
         var error: JSONModelError? = nil
         let result = T.init(string: json, error: &error)
 
         if let error = error {
-            print("TradeItResultTransformer Error")
-            print("Expected class: \(T.self)")
-            print("JSONModel error: \(error)")
-            print("Server response: \(String(describing: json))")
+            print("TradeItResultTransformer")
+            print("- Expected class: \(T.self)")
+            print("- Server response: \(json ?? "")")
+            print("- JSONModel response: \(error)")
             return nil
         } else if let result = result {
             return result
         } else {
-            print("TradeItResultTransformer Error")
-            print("Expected class: \(T.self)")
-            print("Server response: \(String(describing: json))")
+            print("TradeItResultTransformer")
+            print("- Expected class: \(T.self)")
+            print("- Server response: \(json ?? "")")
             return nil
         }
     }

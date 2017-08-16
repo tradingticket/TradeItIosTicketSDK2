62849c74671cb4f9808878d6718bc9078ad0fcc7
Debavelaere <guillaume.debavelaere@gmail.com>
2017-08-16 09:49:48 -0400

    [#150187441] pentest encrypt cached data

--- TradeItIosTicketSDK2/TradeItLinkedBrokerCache.swift
+++ TradeItIosTicketSDK2/TradeItLinkedBrokerCache.swift
@@ -1,3 +1,5 @@
+import CryptoSwift
+
 class TradeItLinkedBrokerCache {
     typealias UserId = String
     typealias SerializedLinkedBroker = [String: Any]
@@ -26,6 +28,20 @@ class TradeItLinkedBrokerCache {
         get {
             return TradeItLinkedBrokerCache._userDefaults
         }
+    }
+    
+    private static let dateFormatter = DateFormatter()
+    private static let numberFormatter = NumberFormatter()
+    private static let TRADEIT_CRYPTO_UUID_KEY = "TRADEIT_CRYPTO_UUID_KEY"
+    private var aesKey: AES!
+    
+    init() {
+        TradeItLinkedBrokerCache.numberFormatter.maximumFractionDigits = 6
+        TradeItLinkedBrokerCache.dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss Z"
+        guard let aesKey = self.getAesKey() else {
+            preconditionFailure("TradeItIosTicketSDK ERROR: TradeItLinkedBrokerCache - unable to get an AES key.")
+        }
+        self.aesKey = aesKey
     }
 
     func cache(linkedBroker: TradeItLinkedBroker?) {
@@ -82,7 +98,7 @@ class TradeItLinkedBrokerCache {
     }
 
     private func serializeAccountEnabled(isEnabled: Bool) -> String {
-        return isEnabled ? ACCOUNT_ENABLED_VALUE : ACCOUNT_DISABLED_VALUE
+        return encryptData(isEnabled ? ACCOUNT_ENABLED_VALUE : ACCOUNT_DISABLED_VALUE)
     }
 
     private func serialize(linkedBroker: TradeItLinkedBroker) -> SerializedLinkedBroker {
@@ -103,28 +119,59 @@ class TradeItLinkedBrokerCache {
                              forLinkedBroker linkedBroker: TradeItLinkedBroker) -> [TradeItLinkedBrokerAccount] {
 
         return serializedAccounts.map { serializedAccount in
+            var balanceLastUpdatedDate:Date? = nil
+            
+            switch serializedAccount[BALANCE_LAST_UPDATED_KEY] {
+            case let balanceLastUpdated as Date:
+                balanceLastUpdatedDate = balanceLastUpdated
+            case let balanceLasUpdatedEncrypted as String:
+                balanceLastUpdatedDate = TradeItLinkedBrokerCache.dateFormatter.date(from:decryptData(balanceLasUpdatedEncrypted))
+            default:
+                balanceLastUpdatedDate = nil
+            }
+        
             let account = TradeItLinkedBrokerAccount(
                 linkedBroker: linkedBroker,
-                accountName: serializedAccount[ACCOUNT_NAME_KEY] as? String  ?? "",
-                accountNumber: serializedAccount[ACCOUNT_NUMBER_KEY]  as? String ?? "",
-                accountBaseCurrency: serializedAccount[ACCOUNT_BASE_CURRENCY_KEY]  as? String ?? "USD",
-                balanceLastUpdated: serializedAccount[BALANCE_LAST_UPDATED_KEY] as? Date,
+                accountName: decryptData(serializedAccount[ACCOUNT_NAME_KEY] as? String  ?? ""),
+                accountNumber: decryptData(serializedAccount[ACCOUNT_NUMBER_KEY]  as? String ?? ""),
+                accountBaseCurrency: decryptData(serializedAccount[ACCOUNT_BASE_CURRENCY_KEY]  as? String ?? "USD"),
+                balanceLastUpdated: balanceLastUpdatedDate,
                 balance: nil,
                 fxBalance: nil,
                 positions: [],
-                isEnabled: deserialize(accountEnabled: serializedAccount[ACCOUNT_ENABLED_KEY] as? String)
+                isEnabled: deserialize(accountEnabled: decryptData(serializedAccount[ACCOUNT_ENABLED_KEY] as? String))
             )
 
-            if let buyingPower = serializedAccount[BALANCE_BUYING_POWER_KEY] as? NSNumber {
+            var buyingPowerNumber:NSNumber? = nil
+            switch serializedAccount[BALANCE_BUYING_POWER_KEY] {
+            case let buyingPower as NSNumber:
+                buyingPowerNumber = buyingPower
+            case let buyingPowerEncrypted as String:
+                buyingPowerNumber = TradeItLinkedBrokerCache.numberFormatter.number(from:decryptData(buyingPowerEncrypted))
+            default:
+                buyingPowerNumber = nil
+            }
+            
+            if let buyingPower = buyingPowerNumber {
                 let balance = TradeItAccountOverview()
                 balance.buyingPower = buyingPower
-                let buyingPowerLabel = serializedAccount[BALANCE_BUYING_POWER_LABEL_KEY] as? String ?? "Buying Power"
+                let buyingPowerLabel = decryptData(serializedAccount[BALANCE_BUYING_POWER_LABEL_KEY] as? String ?? "Buying Power")
                 balance.buyingPowerLabel = buyingPowerLabel
                 balance.accountBaseCurrency = account.accountBaseCurrency
                 account.balance = balance
             }
 
-            if let fxBuyingPower = serializedAccount[FX_BALANCE_BUYING_POWER_KEY] as? NSNumber {
+            var fxBuyingPowerNumber:NSNumber? = nil
+            switch serializedAccount[FX_BALANCE_BUYING_POWER_KEY] {
+            case let fxBuyingPower as NSNumber:
+                fxBuyingPowerNumber = fxBuyingPower
+            case let fxBuyingPowerEncrypted as String:
+                fxBuyingPowerNumber = TradeItLinkedBrokerCache.numberFormatter.number(from:decryptData(fxBuyingPowerEncrypted))
+            default:
+                fxBuyingPowerNumber = nil
+            }
+            
+            if let fxBuyingPower = fxBuyingPowerNumber {
                 let fxBalance = TradeItFxAccountOverview()
                 fxBalance.buyingPowerBaseCurrency = fxBuyingPower
 
@@ -139,32 +186,32 @@ class TradeItLinkedBrokerCache {
         var serializeAccountsList: [SerializedLinkedBrokerAccount] = []
         for account in accounts {
             var serializedAccount = SerializedLinkedBrokerAccount()
-            serializedAccount[ACCOUNT_NAME_KEY] = account.accountName
-            serializedAccount[ACCOUNT_NUMBER_KEY] = account.accountNumber
-            serializedAccount[ACCOUNT_BASE_CURRENCY_KEY] = account.accountBaseCurrency
-            serializedAccount[ACCOUNT_ENABLED_KEY] = account.isEnabled ? ACCOUNT_ENABLED_VALUE : ACCOUNT_DISABLED_VALUE
+            serializedAccount[ACCOUNT_NAME_KEY] = encryptData(account.accountName)
+            serializedAccount[ACCOUNT_NUMBER_KEY] = encryptData(account.accountNumber)
+            serializedAccount[ACCOUNT_BASE_CURRENCY_KEY] = encryptData(account.accountBaseCurrency)
+            serializedAccount[ACCOUNT_ENABLED_KEY] = encryptData(account.isEnabled ? ACCOUNT_ENABLED_VALUE : ACCOUNT_DISABLED_VALUE)
 
             if let balance = account.balance,
                 let buyingPower = balance.buyingPower {
 
                 if let balanceLastUpdated = account.balanceLastUpdated {
-                    serializedAccount[BALANCE_LAST_UPDATED_KEY] = balanceLastUpdated
+                    serializedAccount[BALANCE_LAST_UPDATED_KEY] = encryptData(TradeItLinkedBrokerCache.dateFormatter.string(from: balanceLastUpdated))
                 }
                 
-                serializedAccount[BALANCE_BUYING_POWER_KEY] = buyingPower
+                serializedAccount[BALANCE_BUYING_POWER_KEY] = encryptData(TradeItLinkedBrokerCache.numberFormatter.string(from: buyingPower))
                 
                 let buyingPowerLabel = balance.buyingPowerLabel ?? "Buying Power"
-                serializedAccount[BALANCE_BUYING_POWER_LABEL_KEY] = buyingPowerLabel
+                serializedAccount[BALANCE_BUYING_POWER_LABEL_KEY] = encryptData(buyingPowerLabel)
             }
 
             if let fxBalance = account.fxBalance,
                 let buyingPowerBaseCurrency = fxBalance.buyingPowerBaseCurrency {
 
                 if let balanceLastUpdated = account.balanceLastUpdated {
-                    serializedAccount[BALANCE_LAST_UPDATED_KEY] = balanceLastUpdated
+                    serializedAccount[BALANCE_LAST_UPDATED_KEY] = encryptData(TradeItLinkedBrokerCache.dateFormatter.string(from: balanceLastUpdated))
                 }
 
-                serializedAccount[FX_BALANCE_BUYING_POWER_KEY] = buyingPowerBaseCurrency
+                serializedAccount[FX_BALANCE_BUYING_POWER_KEY] = encryptData(TradeItLinkedBrokerCache.numberFormatter.string(from: buyingPowerBaseCurrency))
             }
 
             serializeAccountsList.append(serializedAccount)
@@ -172,4 +219,56 @@ class TradeItLinkedBrokerCache {
         
         return serializeAccountsList
     }
+    
+    private func encryptData(_ data: String?) -> String {
+        var encryptedData = data ?? ""
+        do {
+            if let data = data {
+                encryptedData = try self.aesKey.encrypt(Array(data.utf8)).toBase64()!
+            }
+        } catch {
+            print("===> ERROR encryptData: \(error)")
+        }
+        return encryptedData
+    }
+    
+    private func decryptData(_ data: String?) -> String {
+        var decryptedData = data ?? ""
+        do {
+            if let data = data {
+                decryptedData = try data.decryptBase64ToString(cipher: self.aesKey)
+            }
+        } catch {
+            print("===> ERROR decryptData: \(error)")
+        }
+        
+        return decryptedData
+    }
+    
+    private func getAesKey() -> AES? {
+        print("(computing key \(Date())")
+        let uuid = getUUID()
+        let index = uuid.index(uuid.startIndex, offsetBy: 16)
+        let salt: Array<UInt8> = Array(uuid.substring(to: index).utf8)
+        var aes: AES?
+        do {
+            let cryptoKey = try PKCS5.PBKDF2(password: Array(uuid.utf8), salt: salt, iterations: 4096, variant: .sha256).calculate()
+            print("(end computing key \(Date())")
+            aes = try AES(key: cryptoKey)
+        } catch {
+            print("===> ERROR Creating cryptoKey: \(error)")
+        }
+        return aes
+    }
+    
+    private func getUUID() -> String {
+        if let uuid = TradeItKeychain.getStringForKey(TradeItLinkedBrokerCache.TRADEIT_CRYPTO_UUID_KEY) {
+            return uuid
+        } else {
+            let newUUID = UUID().uuidString.replacingOccurrences(of: "-", with: "")
+            TradeItKeychain.save(newUUID, forKey: TradeItLinkedBrokerCache.TRADEIT_CRYPTO_UUID_KEY)
+            return newUUID
+        }
+    }
+    
 }

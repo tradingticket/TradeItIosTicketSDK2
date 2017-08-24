94f6575b2cb5ed20cd91c5ca173401801c7411cc
Alex Kramer <alex@trade.it>
2017-08-24 13:06:22 -0400

    Extracts private method to generate keychain key for caching linked broker

--- TradeItIosTicketSDK2/TradeItLinkedBrokerCache.swift
+++ TradeItIosTicketSDK2/TradeItLinkedBrokerCache.swift
@@ -1,10 +1,8 @@
 class TradeItLinkedBrokerCache {
-
-    private let LINKED_BROKER_CACHE_KEY = "TRADE_IT_LINKED_BROKER_CACHE_"
   
     func cache(linkedBroker: TradeItLinkedBroker?) {
         guard let linkedBroker = linkedBroker else { return }
-        let key = LINKED_BROKER_CACHE_KEY+linkedBroker.userId
+        let key = self.key(forLinkeBroker: linkedBroker)
         
         let cachedLinkedBroker = CachedLinkedBroker(linkedBroker: linkedBroker)
         TradeItKeychain.save(cachedLinkedBroker.toJSONString(), forKey: key)
@@ -13,7 +11,7 @@ class TradeItLinkedBrokerCache {
     }
 
     func syncFromCache(linkedBroker: TradeItLinkedBroker) {
-        let key = LINKED_BROKER_CACHE_KEY+linkedBroker.userId
+        let key = self.key(forLinkeBroker: linkedBroker)
         guard let json = TradeItKeychain.getStringForKey(key)
              ,let cachedLinkedBroker = TradeItResultTransformer.transform(targetClassType: CachedLinkedBroker.self, json: json)
             else { return}
@@ -24,7 +22,7 @@ class TradeItLinkedBrokerCache {
                 accountName: cachedAccount.accountName,
                 accountNumber: cachedAccount.accountNumber,
                 accountIndex: cachedAccount.accountIndex,
-                accountBaseCurrency: cachedAccount.accountBaseCurrency ,
+                accountBaseCurrency: cachedAccount.accountBaseCurrency,
                 balanceLastUpdated: cachedAccount.balanceLastUpdated,
                 balance: cachedAccount.balance,
                 fxBalance: cachedAccount.fxBalance,
@@ -33,9 +31,10 @@ class TradeItLinkedBrokerCache {
                 isEnabled: cachedAccount.isEnabled
             )
         }
+
         linkedBroker.accountsLastUpdated = cachedLinkedBroker.accountsLastUpdated
         linkedBroker.isAccountLinkDelayedError = cachedLinkedBroker.isAccountLinkDelayedError
-        
+
         if linkedBroker.isAccountLinkDelayedError {
             linkedBroker.error = TradeItErrorResult(
                 title: "Activation In Progress",
@@ -46,11 +45,16 @@ class TradeItLinkedBrokerCache {
     }
 
     func remove(linkedBroker: TradeItLinkedBroker) {
-        let key = LINKED_BROKER_CACHE_KEY+linkedBroker.userId
+        let key = self.key(forLinkeBroker: linkedBroker)
         TradeItKeychain.deleteString(forKey: key)
     }
 
     // MARK: Private
+
+    private func key(forLinkeBroker linkedBroker: TradeItLinkedBroker) -> String {
+        let LINKED_BROKER_CACHE_KEY = "TRADE_IT_LINKED_BROKER_CACHE_"
+        return LINKED_BROKER_CACHE_KEY + linkedBroker.userId
+    }
     
     private func removeOldCache(linkedBroker: TradeItLinkedBroker) {
         if let userDefault = UserDefaults(suiteName: "it.trade") {
@@ -63,9 +67,11 @@ class TradeItLinkedBrokerCache {
 extension CachedLinkedBroker {
     convenience init(linkedBroker: TradeItLinkedBroker) {
         self.init()
+
         self.accounts = linkedBroker.accounts.map { account in
             return CachedLinkedBrokerAccount(linkedBrokerAccount: account)
         }
+
         self.accountsLastUpdated = linkedBroker.accountsLastUpdated
         self.isAccountLinkDelayedError = linkedBroker.isAccountLinkDelayedError
     }

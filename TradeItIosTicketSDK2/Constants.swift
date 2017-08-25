33f14944401909b2aedd2ef2ce3e6d65b80d37bb
Alex Kramer <alex@trade.it>
2017-08-25 17:10:38 -0400

    Adds constants file with TradeItNotificationKey for notification center keys

---
+++ TradeItIosTicketSDK2/Constants.swift
@@ -0,0 +1,9 @@
+struct TradeItNotificationKey {
+    // TODO: Move existing notification center constants here
+
+    struct UserEvent {
+        struct Yahoo {
+            static let yahooOAuthCompletionViewLoaded =  NSNotification.Name(rawValue: "yahooOAuthCompletionViewLoaded")
+        }
+    }
+}

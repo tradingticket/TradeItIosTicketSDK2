f8ff968b4b44dcbfe4d748f0593da256159fd2db
Debavelaere <guillaume.debavelaere@gmail.com>
2017-08-31 15:20:28 -0400

    [#150685848] Refactor TradeItPreviewTradeOrderDetails+Helpers to provide string value from enum and enum from string value

--- TradeItIosTicketSDK2/TradeItPreviewTradeOrderDetails+Helpers.swift
+++ TradeItIosTicketSDK2/TradeItPreviewTradeOrderDetails+Helpers.swift
@@ -1,31 +1,122 @@
-@objc public enum TradeItOrderExpiration: Int {
+public typealias RawValue = String
+
+@objc public enum TradeItOrderExpiration: Int, RawRepresentable {
     case goodForDay
     case goodUntilCanceled
     case unknown
+    
+    public var rawValue: RawValue {
+        switch self {
+        case .goodForDay:
+            return "day"
+        case .goodUntilCanceled:
+            return "gtc"
+        default:
+            return "unknown"
+        }
+    }
+    
+    public init?(rawValue: RawValue) {
+        self.init(value: rawValue)
+    }
+    
+    public init(value: RawValue?) {
+        let rawValue = value ?? "unknown"
+        switch rawValue {
+        case "day":
+            self = .goodForDay
+        case "gtc":
+            self = .goodUntilCanceled
+        default:
+            self = .unknown
+        }
+    }
+
 }
 
-@objc public enum TradeItOrderPriceType: Int {
+@objc public enum TradeItOrderPriceType: Int, RawRepresentable {
     case market
     case limit
     case stopMarket
     case stopLimit
     case unknown
+    
+    public var rawValue: RawValue {
+        switch self {
+        case .market:
+            return "market"
+        case .limit:
+            return "limit"
+        case .stopMarket:
+            return "stopMarket"
+        case .stopLimit:
+            return "stopLimit"
+        default:
+            return "unknown"
+        }
+    }
+    
+    public init?(rawValue: RawValue) {
+        self.init(value: rawValue)
+    }
+    
+    public init(value: RawValue?) {
+        let rawValue = value ?? "unknown"
+        switch rawValue {
+        case "market":
+            self = .market
+        case "limit":
+            self = .limit
+        case "stopMarket":
+            self = .stopMarket
+        case "stopLimit":
+            self = .stopLimit
+        default:
+            self = .unknown
+        }
+    }
+
 }
 
-@objc public enum TradeItOrderAction: Int {
+@objc public enum TradeItOrderAction: Int, RawRepresentable {
     case buy
     case sell
     case buyToCover
     case sellShort
     case unknown
-}
-
-extension TradeItPreviewTradeOrderDetails {
-    func expirationType() -> TradeItOrderExpiration {
-        switch self.orderExpiration {
-        case "day": return .goodForDay
-        case "gtc": return .goodUntilCanceled
-        default: return .unknown
+    
+    public var rawValue: RawValue {
+        switch self {
+        case .buy:
+            return "buy"
+        case .sell:
+            return "sell"
+        case .buyToCover:
+            return "buyToCover"
+        case .sellShort:
+            return "sellShort"
+        default:
+            return "unknown"
+        }
+    }
+    
+    public init?(rawValue: RawValue) {
+        self.init(value: rawValue)
+    }
+    
+    public init(value: RawValue?) {
+        let rawValue = value ?? "unknown"
+        switch rawValue {
+        case "buy":
+            self = .buy
+        case "sell":
+            self = .sell
+        case "buyToCover":
+            self = .buyToCover
+        case "sellShort":
+            self = .sellShort
+        default:
+            self = .unknown
         }
     }
 }

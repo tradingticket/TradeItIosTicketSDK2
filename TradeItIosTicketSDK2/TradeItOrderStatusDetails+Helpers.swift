f86379751416a919b0fe87dc781b4ea56a5ac72c
Debavelaere <guillaume.debavelaere@gmail.com>
2017-09-14 17:25:18 +0200

    [#149410359] Refactor TradeItorderDetails extension to handle partially filled group orders

--- TradeItIosTicketSDK2/TradeItOrderStatusDetails+Helpers.swift
+++ TradeItIosTicketSDK2/TradeItOrderStatusDetails+Helpers.swift
@@ -1,26 +1,34 @@
 extension TradeItOrderStatusDetails {
-    
-    private var openOrderStatus: [String] {
-        return  ["PENDING", "OPEN", "PART_FILLED", "PENDING_CANCEL"]
+    private typealias Category = [String]
+    private var openOrderCategory: Category {
+        return  ["PENDING", "OPEN", "PENDING_CANCEL"]
     }
     
-    private var filledOrderStatus: [String] {
+    private var partiallyFilledCategory: Category {
+        return ["PART_FILLED"]
+    }
+    
+    private var filledOrderCategory: Category {
         return ["FILLED"]
     }
-    private var otherOrderStatus: [String] {
+    private var otherOrderCategory: Category {
         return ["CANCELED", "REJECTED", "NOT_FOUND", "EXPIRED"]
     }
     
-    func containsOpenStatus() -> Bool {
-        return isOrderStatusOneOf(orderStatus: openOrderStatus)
+    func belongsToOpenCategory() -> Bool {
+        return belongsToCategory(orderCategory: openOrderCategory)
     }
     
-    func containsFilledStatus() -> Bool {
-        return isOrderStatusOneOf(orderStatus: filledOrderStatus)
+    func belongsToFilledCategory() -> Bool {
+        return belongsToCategory(orderCategory: filledOrderCategory)
     }
     
-    func containsOtherOrderStatus() -> Bool {
-        return isOrderStatusOneOf(orderStatus: otherOrderStatus)
+    func belongsToOtherCategory() -> Bool {
+        return belongsToCategory(orderCategory: otherOrderCategory)
+    }
+    
+    func belongsToPartiallyFilledCategory() -> Bool {
+        return belongsToCategory(orderCategory: partiallyFilledCategory)
     }
     
     func isGroupOrder() -> Bool {
@@ -30,14 +38,30 @@ extension TradeItOrderStatusDetails {
     
     // MARK: private
     
-    private func isOrderStatusOneOf(orderStatus: [String]) -> Bool {
-        let containsSimpleStatusOrders = orderStatus.contains(self.orderStatus ?? "")
-        
+    private func belongsToCategory(orderCategory: Category) -> Bool {
         guard let groupOrders = self.groupOrders, groupOrders.count > 0 else {
-            return containsSimpleStatusOrders
+            return orderCategory.contains(self.orderStatus ?? "")
         }
         
-        return groupOrders.filter { $0.isOrderStatusOneOf(orderStatus: orderStatus) }.count > 0
+        //Group orders specificity
+        if orderCategory == partiallyFilledCategory { // Group orders belong to partially filled category if at least one order is filled and one other is different than filled
+            let belongsToFilledOrder = groupOrders.filter { $0.belongsToFilledCategory() }.count > 0
+            let belongsToOtherThanFilledOrders = groupOrders.filter { !$0.belongsToFilledCategory() }.count > 0
+            return belongsToFilledOrder && belongsToOtherThanFilledOrders
+        } else if orderCategory == otherOrderCategory { // Group orders belong to otherOrderCategory category if at least 2 legs are different and not filled
+            let belongsToOpenCategory = groupOrders.filter { $0.belongsToOpenCategory() }.count > 0
+            let belongsToPartiallyFilledCategory = groupOrders.filter { $0.belongsToPartiallyFilledCategory() }.count > 0
+            let belongsToOtherCategory = groupOrders.filter { $0.belongsToOtherCategory() }.count > 0
+            let belongsToDifferentCategoriesOtherThanFilled = ( belongsToOpenCategory && belongsToOtherCategory
+                || belongsToOpenCategory && belongsToPartiallyFilledCategory
+                || belongsToOtherCategory && belongsToPartiallyFilledCategory
+            )
+            let belongsToFilledCategory = groupOrders.filter { $0.belongsToFilledCategory() }.count > 0
+            return belongsToDifferentCategoriesOtherThanFilled && !belongsToFilledCategory
+        } else { // Group orders belong to a category if all of the legs belong to the same category
+            let belongsToCategory = groupOrders.filter { $0.belongsToCategory(orderCategory: orderCategory) }.count > 0
+            let belongsToAnOtherCategory = groupOrders.filter { !$0.belongsToCategory(orderCategory: orderCategory) }.count > 0
+            return belongsToCategory && !belongsToAnOtherCategory
+        }
     }
-
 }

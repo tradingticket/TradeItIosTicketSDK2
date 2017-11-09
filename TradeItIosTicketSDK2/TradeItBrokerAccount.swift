d86a4bfec9a0e66a45e66080278343ef86bc9ceb
guillaumedebavelaere <guillaume@trade.it>
2017-11-09 11:51:36 -0500

    [#151998219] FidelityV1 - toogle margin - TradeItBrokerAccount - add extension to add an enum for marginType

--- TradeItIosTicketSDK2/TradeItBrokerAccount.swift
+++ TradeItIosTicketSDK2/TradeItBrokerAccount.swift
@@ -1,13 +1,61 @@
-//
-//  TradeITBrokerAccount.swift
-//  TradeItIosTicketSDK2
-//
-//  Created by Guillaume Debavelaere on 11/9/17.
-//  Copyright © 2017 TradeIt. All rights reserved.
-//
+@objc public enum TradeItMarginType: Int, RawRepresentable {
+    case margin
+    case cash
+    case null
+    
+    public var rawValue: RawValue {
+        switch self {
+        case .margin:
+            return "MARGIN"
+        case .cash:
+            return "CASH"
+        default:
+            return ""
+        }
+    }
+    
+    public var label: String {
+        switch self {
+        case .margin:
+            return "Margin"
+        case .cash:
+            return "Cash"
+        default:
+            return ""
+        }
+    }
+    
+    public init?(rawValue: RawValue) {
+        self.init(value: rawValue)
+    }
+    
+    public init(value: RawValue?) {
+        let rawValue = value ?? ""
+        switch rawValue {
+        case "MARGIN":
+            self = .margin
+        case "CASH":
+            self = .cash
+        default:
+            self = .null
+        }
+    }
+    
+    static func valueFor(label: String) -> TradeItMarginType {
+        switch label {
+        case "Margin":
+            return .margin
+        case "Cash":
+            return .cash
+        default:
+            return .null
+        }
+    }
+}
 
-import UIKit
+extension TradeItBrokerAccount {
 
-class TradeITBrokerAccount: NSObject {
-
+    var marginTypeEnum: TradeItMarginType {
+        return TradeItMarginType(value: self.marginType)
+    }
 }

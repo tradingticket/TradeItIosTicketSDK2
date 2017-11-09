b4a4539eeb1ac87dda6998c026bde9c61cadbb80
guillaumedebavelaere <guillaume@trade.it>
2017-11-09 11:50:38 -0500

    [#151998219] FidelityV1 - toogle margin - add marginType to the account response

---
+++ TradeItIosTicketSDK2/TradeItBrokerAccount.swift
@@ -0,0 +1,13 @@
+//
+//  TradeITBrokerAccount.swift
+//  TradeItIosTicketSDK2
+//
+//  Created by Guillaume Debavelaere on 11/9/17.
+//  Copyright © 2017 TradeIt. All rights reserved.
+//
+
+import UIKit
+
+class TradeITBrokerAccount: NSObject {
+
+}

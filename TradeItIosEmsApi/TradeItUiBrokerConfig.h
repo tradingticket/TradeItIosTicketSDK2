207c7f0caa40c48ddf635a3fc59e42a3738ffba3
James Robert Somers and Oliver Scott <pair+james+oliver@trade.it>
2018-04-03 15:16:55 -0400

    add request/response layer for ems uiConfig endpoint

--- TradeItIosEmsApi/TradeItUiBrokerConfig.h
+++ TradeItIosEmsApi/TradeItUiBrokerConfig.h
@@ -1,9 +1,12 @@
 #import <JSONModel/JSONModel.h>
 #import "TradeItBrokerLogo.h"
 
+@protocol TradeItUiBrokerConfig
+@end
+
 @interface TradeItUiBrokerConfig : JSONModel
 
-//@property(nonatomic, copy, nonnull) NSString* brokerId;
+@property(nonatomic, copy, nonnull) NSString* brokerId;
 @property(nonatomic, copy, nonnull) NSArray<TradeItBrokerLogo> *logos;
 
 @end

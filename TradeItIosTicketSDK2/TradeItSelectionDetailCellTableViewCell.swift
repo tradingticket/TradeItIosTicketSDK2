e76a939f65422241317d34cb6a06b417647e6b2a
James Robert Somers and Oliver Scott <pair+james+oliver@trade.it>
2018-04-06 15:35:41 -0400

    add branding to trading ticket

--- TradeItIosTicketSDK2/TradeItSelectionDetailCellTableViewCell.swift
+++ TradeItIosTicketSDK2/TradeItSelectionDetailCellTableViewCell.swift
@@ -3,14 +3,23 @@ import UIKit
 class TradeItSelectionDetailCellTableViewCell: UITableViewCell {
     @IBOutlet weak var detailPrimaryLabel: UILabel!
     @IBOutlet weak var detailSecondaryLabel: UILabel!
-
+    @IBOutlet weak var logo: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         self.accessoryView = DisclosureIndicator()
     }
 
-    func configure(detailPrimaryText: String?, detailSecondaryText: String?) {
+    func configure(detailPrimaryText: String?, detailSecondaryText: String?, linkedBroker: TradeItLinkedBroker? = nil) {
         self.detailPrimaryLabel.text = detailPrimaryText
         self.detailSecondaryLabel.text = detailSecondaryText
+        TradeItSDK.brokerLogoService.loadLogo(
+            forBrokerId: linkedBroker?.brokerName,
+            withSize: .small,
+            onSuccess: { image in
+                self.logo.image = image
+            }, onFailure: { 
+            }
+        )
     }
 }

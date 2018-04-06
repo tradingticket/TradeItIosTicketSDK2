000ec7fb8b5e3c4925138c880cdf24f8b6aef02c
James Robert Somers and Oliver Scott <pair+james+oliver@trade.it>
2018-04-06 13:34:39 -0400

    abstract the branded table view broker header and add to Trading -> Account Selection Sceen

--- TradeItIosTicketSDK2/TradeItBundleProvider.swift
+++ TradeItIosTicketSDK2/TradeItBundleProvider.swift
@@ -1,8 +1,16 @@
-@objc public class TradeItBundleProvider: NSObject {
+@objc public class TradeItBundleProvider: NSObject { // NextStep
     static public func provide() -> Bundle {
         let framework = Bundle.init(for: self)
         let bundlePathOptional = framework.path(forResource: "TradeItIosTicketSDK2", ofType: "bundle")
         guard let bundlePath = bundlePathOptional, let bundle = Bundle.init(path: bundlePath) else { return framework }
         return bundle
+    }
+    
+    static func registerBrokerHeaderNibCells(forTableView tableView: UITableView) {
+        let bundle = provide()
+        tableView.register(
+            UINib(nibName: "TradeItBrokerHeaderTableViewCell", bundle: bundle),
+            forCellReuseIdentifier: "TRADE_IT_BROKER_HEADER"
+        )
     }
 }

4fc915b4c77bffc4e000547e9b66a4e7f7c02625
Oliver Scott <oliverhbscott@gmail.com>
2018-04-10 14:51:20 -0400

    add branding to preview screen

--- TradeItIosTicketSDK2/TradeItBundleProvider.swift
+++ TradeItIosTicketSDK2/TradeItBundleProvider.swift
@@ -1,4 +1,4 @@
-@objc public class TradeItBundleProvider: NSObject { // NextStep
+@objc public class TradeItBundleProvider: NSObject {
     static public func provide() -> Bundle {
         let framework = Bundle.init(for: self)
         let bundlePathOptional = framework.path(forResource: "TradeItIosTicketSDK2", ofType: "bundle")
@@ -11,6 +11,22 @@
         tableView.register(
             UINib(nibName: "TradeItBrokerHeaderTableViewCell", bundle: bundle),
             forCellReuseIdentifier: "TRADE_IT_BROKER_HEADER"
+        )
+    }
+
+    static func registerPreviewMessageNibCells(forTableView tableView: UITableView) {
+        let bundle = provide()
+        tableView.register(
+            UINib(nibName: "TradeItPreviewMessageTableViewCell", bundle: bundle),
+            forCellReuseIdentifier: "PREVIEW_MESSAGE_CELL_ID"
+        )
+    }
+
+    static func registerPreviewBrandedAccountNibCells(forTableView tableView: UITableView) {
+        let bundle = provide()
+        tableView.register(
+            UINib(nibName: "TradeItPreviewBrandedAccountNameCell", bundle: bundle),
+            forCellReuseIdentifier: "PREVIEW_BRANDED_ACCOUNT_NAME_CELL_ID"
         )
     }
 }

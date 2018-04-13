4034206c2bea78b4d89fda6f5fd67fedb06113f6
Oliver Scott <oliverhbscott@gmail.com>
2018-04-13 10:18:27 -0400

    1. Remove 'Account' label from Branded...Cell used in Preview Screen and replace with Broker Logo if successfully fetched else Broker Display Name 2. Add horizontal spacing between label and account number

--- TradeItIosTicketSDK2/TradeItPreviewBrandedAccountNameCell.swift
+++ TradeItIosTicketSDK2/TradeItPreviewBrandedAccountNameCell.swift
@@ -3,21 +3,30 @@ import UIKit
 class TradeItPreviewBrandedAccountNameCell: UITableViewCell {
     @IBOutlet weak var accountName: UILabel!
     @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var logoWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var brokerName: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        self.accessoryView = DisclosureIndicator()
+        TradeItThemeConfigurator.configure(view: self)
     }
     
     func populate(linkedBroker: TradeItLinkedBrokerAccount) {
         self.accountName.text = linkedBroker.getFormattedAccountName()
+        self.brokerName.text = linkedBroker.brokerLongName
         TradeItSDK.brokerLogoService.loadLogo(
             forBrokerId: linkedBroker.brokerName,
             withSize: .small,
             onSuccess: { image in
+                let imageWidth = Double(image.cgImage?.width ?? 1)
+                let imageHeight = Double(image.cgImage?.height ?? 1)
+                self.logoWidthConstraint.constant = CGFloat(Double(15) * imageWidth / imageHeight)
                 self.logo.image = image
-        }, onFailure: {
-        }
+                self.brokerName.isHidden = true
+            },
+            onFailure: {
+                self.logo.isHidden = true
+            }
         )
     }
 }

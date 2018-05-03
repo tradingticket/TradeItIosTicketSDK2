e7dbecc974dce475269c6f0271d49cc860db493f
James Robert Somers <james@trade.it>
2018-05-03 11:52:11 -0400

    Change text Accounts -> Account on Yahoo tickets

--- TradeItIosTicketSDK2/TradeItYahooTradingTicketViewController.swift
+++ TradeItIosTicketSDK2/TradeItYahooTradingTicketViewController.swift
@@ -580,7 +580,7 @@ class TradeItYahooTradingTicketViewController: TradeItYahooViewController, UITab
             case .orderType: return "Order type"
             case .expiration: return "Time in force"
             case .marketPrice: return "Market price"
-            case .account: return "Accounts"
+            case .account: return "Account"
             case .marginType: return "Type"
             }
         }

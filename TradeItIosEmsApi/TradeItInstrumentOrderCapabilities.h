e2ba9ea2918372f1733607cda8babad1e6a1301d
James Robert Somers <james@trade.it>
2018-05-03 11:51:56 -0400

    Add InstrumentActionCapability

--- TradeItIosEmsApi/TradeItInstrumentOrderCapabilities.h
+++ TradeItIosEmsApi/TradeItInstrumentOrderCapabilities.h
@@ -1,5 +1,6 @@
 #import <JSONModel/JSONModel.h>
 #import "TradeItInstrumentCapability.h"
+#import "TradeItInstrumentActionCapability.h"
 
 @protocol TradeItInstrumentOrderCapabilities
 
@@ -10,7 +11,7 @@
 @property (nonatomic, copy) NSString * _Nonnull instrument;
 @property (nonatomic, copy) NSString<Optional> * _Nullable tradeItSymbol;
 @property (nonatomic, copy) NSNumber<Optional> * _Nullable precision;
-@property (nonatomic, copy) NSArray<Optional, TradeItInstrumentCapability> * _Nullable actions;
+@property (nonatomic, copy) NSArray<Optional, TradeItInstrumentActionCapability> * _Nullable actions;
 @property (nonatomic, copy) NSArray<Optional, TradeItInstrumentCapability> * _Nullable expirationTypes;
 @property (nonatomic, copy) NSArray<Optional, TradeItInstrumentCapability> * _Nullable priceTypes;
 @property (nonatomic, copy) NSArray<Optional, TradeItInstrumentCapability> * _Nullable orderTypes;
